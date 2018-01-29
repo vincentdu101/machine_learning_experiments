13805e8e785dd880d1580188cade2cddf6c93765
vincentdu101 <vincentdu101@gmail.com>
2018-01-29 00:16:09 -0800

    finish progress on svr

--- Part 2 - Regression/Section 7 - Support Vector Regression (SVR)/experiments/svr.R
+++ Part 2 - Regression/Section 7 - Support Vector Regression (SVR)/experiments/svr.R
@@ -1,4 +1,4 @@
-# Regression Template
+# SVR
 
 # Importing the dataset
 dataset = read.csv('Position_Salaries.csv')
@@ -17,12 +17,17 @@ dataset = dataset[2:3]
 # test_set = scale(test_set)
 
 # Fitting the Regression Model to the dataset
-# Create your regressor here
+# install.packages('e1071')
+library(e1071)
+# type specifies whether it is a regression model 
+regressor = svm(formula = Salary ~ .,
+                data = dataset,
+                type = "eps-regression")
 
-# Predicting a new result
+# Predicting a new result given a certain level
 y_pred = predict(regressor, data.frame(Level = 6.5))
 
-# Visualising the Regression Model results
+# Visualising the SVR results
 # install.packages('ggplot2')
 library(ggplot2)
 ggplot() +
@@ -30,19 +35,6 @@ ggplot() +
              colour = 'red') +
   geom_line(aes(x = dataset$Level, y = predict(regressor, newdata = dataset)),
             colour = 'blue') +
-  ggtitle('Truth or Bluff (Regression Model)') +
-  xlab('Level') +
-  ylab('Salary')
-
-# Visualising the Regression Model results (for higher resolution and smoother curve)
-# install.packages('ggplot2')
-library(ggplot2)
-x_grid = seq(min(dataset$Level), max(dataset$Level), 0.1)
-ggplot() +
-  geom_point(aes(x = dataset$Level, y = dataset$Salary),
-             colour = 'red') +
-  geom_line(aes(x = x_grid, y = predict(regressor, newdata = data.frame(Level = x_grid))),
-            colour = 'blue') +
-  ggtitle('Truth or Bluff (Regression Model)') +
+  ggtitle('Truth or Bluff (SVR)') +
   xlab('Level') +
   ylab('Salary')
--- Part 2 - Regression/Section 7 - Support Vector Regression (SVR)/experiments/svr.py
+++ Part 2 - Regression/Section 7 - Support Vector Regression (SVR)/experiments/svr.py
@@ -1,4 +1,4 @@
-# Support Vector Regression (SVR)
+# SVR
 
 # Importing the libraries
 import numpy as np
@@ -10,74 +10,40 @@ dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
 
-# feature scaling - normalize values so they are in the same range for measurement
-# easier to have models analyze differences and not skew results 
-# for svr we need to use it because this svr class does not have include feature
-# scaling
+# Splitting the dataset into the Training set and Test set
+"""from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
+
+# Feature Scaling
 from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 sc_y = StandardScaler()
 X = sc_X.fit_transform(X)
 y = sc_y.fit_transform(y)
 
-# we do not need to do feature scaling to the y variable column
-
-# fitting the new regression model to the dataset
+# Fitting SVR to the dataset
 from sklearn.svm import SVR
-regressor = SVR(kernel = "rbf").fit(X, y)
+regressor = SVR(kernel = 'rbf')
+regressor.fit(X, y)
 
-# predicting a new result with svr
-# need to transform it since the current regressor is using x and y coordinates
+# Predicting a new result
 y_pred = regressor.predict(6.5)
 y_pred = sc_y.inverse_transform(y_pred)
 
-# visualizing the svr results
-# you will see that the CEO salary is an outlier, since its too far from the 
-# other points the line does not follow it completely
+# Visualising the SVR results
 plt.scatter(X, y, color = 'red')
 plt.plot(X, regressor.predict(X), color = 'blue')
-plt.title('Truth or Bluff (SVR Model)')
+plt.title('Truth or Bluff (SVR)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
 plt.show()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Visualising the SVR results (for higher resolution and smoother curve)
+X_grid = np.arange(min(X), max(X), 0.01) # choice of 0.01 instead of 0.1 step because the data is feature scaled
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color = 'red')
+plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
+plt.title('Truth or Bluff (SVR)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
